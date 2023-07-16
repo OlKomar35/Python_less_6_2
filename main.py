e559e54bfebd57b34ef34240ab4e79ef08fd7546
Olga Komar <komar_ol@mail.ru>
2023-07-16 16:30:04 +0300

    added task 2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # TЗадача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не
 # меньше заданного минимума и не больше заданного максимума)
+import random
+
+print(arr := [random.randint(-10,10) for _ in range(int(input('n= ')))])
+min_val_range = int(input('min= '))
+max_val_range = int(input('max= '))
+
+for i in range(len(arr)):
+    if max_val_range >= arr[i] >= min_val_range:
+        print(i, end=' ')
 
